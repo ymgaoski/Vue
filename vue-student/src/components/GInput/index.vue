a23a3b201c59d8c9dfe83045009ffcc760c096af
ymgaoski!1 <ywengao@qq.com>
2020-12-09 16:51:52 +0800

    初始化

---
+++ vue-student/src/components/GInput/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="com-container">
+    <input class="g-input" :value="value" v-bind="$attrs" @input="onInput"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value:{
+      type: String
+    }
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+
+    // 输入框改变
+    onInput(e){
+      this.$emit('input',e.target.value);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.com-container{
+  .g-input{
+    height: 24px;
+    line-height: 24px;
+    outline: none;
+    border: 1px solid rgb(182, 182, 182);
+    border-radius: 4px;
+    padding: 0 5px;
+  }
+}
+</style>
